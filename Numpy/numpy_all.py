08b4584acbc1770678e9b5a8f1fdeaf69e0e5dcc
Ирина <Smolenskaya2002@yandex.ru>
2023-02-13 01:11:53 +0300

    Update numpy_all.py

--- Numpy/numpy_all.py
+++ Numpy/numpy_all.py
@@ -57,3 +57,44 @@ zero_mod_4_mask = 0 == (av_array % 4)
 combined = np.logical_and(zero_mod_2_mask, zero_mod_4_mask)
 print(combined)
 
+print('---------------')
+
+ave_3D_array = np.arange(70)
+ave_3D_array.shape = (2, 7, 5)
+print(ave_3D_array)
+print(5 * ave_3D_array - 10)
+
+left_matrix = np.arange(6).reshape((2, 3))
+right_matrix = np.arange(15).reshape((3, 5))
+# reshape - делает копию массива и изменяет его форму, не изменяя оригинальный массив
+
+print(np.dot(left_matrix, right_matrix)) # перемножение матриц
+
+print('---------------')
+
+ave_2D_array = np.ones(35, dtype='int_').reshape((7, 5)) * 5
+print(ave_2D_array)
+
+print('---------------')
+
+ave_random = np.random.random((7, 5))
+np.set_printoptions(precision=4) # точность до 4 знаков после запятой
+print(ave_random)
+
+print('---------------')
+
+employee_data = [('name', 'S6'), ('height', 'f8'), ('weight', 'f8')]
+print(employee_data)
+
+employees = np.zeros((3), dtype=employee_data)
+print(employees)
+
+employees[2] = ('Dan', 200, 100)
+print(employees)
+
+print('---------------')
+
+trial_array = (np.arange(24)).reshape((2, 3, 4))
+trial_array2 = np.append(trial_array, [5, 6, 7, 8])
+print(trial_array)
+print(trial_array2)
